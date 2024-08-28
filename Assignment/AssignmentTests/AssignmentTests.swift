3e22e0d446f8d2be94ca6c1c94ac840a64ad7516
nagarajukamatham3 <nagaraju.kamatham3@gmail.com>
2024-08-28 22:34:14 +0530

    testFetchUserAPI -

--- Assignment/AssignmentTests/AssignmentTests.swift
+++ Assignment/AssignmentTests/AssignmentTests.swift
@@ -9,28 +9,20 @@ import XCTest
 @testable import Assignment
 
 final class AssignmentTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testFetchUserAPI() {
+        let expection = expectation(description: "Fetch Users data")
+        NetworkManager.shared.fetchUsers(perPage: 12) { result in
+            switch result {
+            case .success(let response):
+                XCTAssertEqual(response?.perPage, 12)
+                XCTAssertNotNil(response?.perPage)
+            case .failure(let error):
+                XCTAssertNil(error)
+            }
+            expection.fulfill()
         }
+        wait(for: [expection], timeout: 5.0)
     }
 
 }
